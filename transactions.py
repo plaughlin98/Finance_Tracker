f767e58e3098de622b32e7049ea75f456bf1accc
plaughlin98 <plaughlin98@gmail.com>
2022-02-11 23:34:23 -0600

    Update Transactions

--- transactions.py
+++ transactions.py
@@ -1,9 +1,10 @@
 class Transaction:
-    def __init__(self, date, amount, desc, trans_id, trans_type):
+    def __init__(self, date, amount, category, trans_id, trans_type):
         self.date = date
         self.amount = amount
-        self.desc = desc
+        self.category = category
         self.trans_id = trans_id
+        self.trans_type = trans_type
 
-    def __repre__(self):
-        return "Transaction('{}', '{}', '{}', '{}', '{}', '{}')".format(self.date, self.amount, self.desc, self.trans_id, self.trans_type)
+    def __repr__(self):
+        return "Transaction('{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(self.date, self.amount, self.category, self.trans_id, self.trans_type)

e5437395cc547e62803e5df1ce4b1c7348336aac
plaughlin98 <plaughlin98@gmail.com>
2022-02-11 22:19:31 -0600

    Add insert and update_amount func

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import sqlite3
 
-conn = sqlite3.connect('finances.db')
+# INSERT'finances.db' AFTER TESTING
+conn = sqlite3.connect(':memory:')
 
 c = conn.cursor()
 
 
 # CREATE TABLES
-
+# EXPENSES
 c.execute(""" CREATE TABLE expenses (
-            expense_date text,
+            date text,
             amount integer,
-            desc text
+            desc text,
+            trans_id text
             )""")
 
+# INCOME
 c.execute(""" CREATE TABLE income (
-            expense_date text,
+            date text,
             amount integer,
-            desc text
+            desc text,
+            trans_id text
             )""")
+
+def insert_trans(trans, table):
+    with conn:
+        c.execute("INSERT INTO :table VALUES (:date, :amount, :desc)", {'date': trans.date, 'amount': trans.amount, 'desc': trans.desc})
+
+def update_amount(trans, amount, table):
+    with conn:
+        c.execute("""UPDATE :table SET amount = :amount
+                    WHERE trans_id = :trans_id""",
+                    {'table': table, 'trans_id': trans.trans_id, 'amount': amount})
+
+def update_date(trans, date, table):
+    with conn:
+        c.connect()
+
+def update_desc(trans, desc, table):
+    pass
+
+def remove_trans(emp, table):
+    pass
+
+def get_trans_by_date(date, table):
+    pass

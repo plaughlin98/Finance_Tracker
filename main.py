b3a056f2d83d11e3ae9991f1b19ed4d62b95c553
plaughlin98 <plaughlin98@gmail.com>
2022-02-11 23:19:29 -0600

    Update Create table

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ c = conn.cursor()
 
 # CREATE TABLES
 # EXPENSES
-c.execute(""" CREATE TABLE expenses (
+c.execute(""" CREATE TABLE [IF NOT EXISTS] expenses (
             date text,
             amount integer,
             desc text,
@@ -17,7 +17,7 @@ c.execute(""" CREATE TABLE expenses (
             )""")
 
 # INCOME
-c.execute(""" CREATE TABLE income (
+c.execute(""" CREATE TABLE [IF NOT EXISTS] income (
             date text,
             amount integer,
             desc text,
@@ -37,23 +37,23 @@ def update_amount(trans, amount, table):
 
 def update_date(trans, date, table):
     with conn:
-        c.connect("""UPDATE :table SET date = :date
+        c.execute("""UPDATE :table SET date = :date
                     WHERE trans_id = :trans_id""",
                     {'table': table, 'date': date, 'trans_id': trans.trans_id})
 
 def update_desc(trans, desc, table):
     with conn:
-        c.connect(""" UPDATE :table SET desc = :desc
+        c.execute(""" UPDATE :table SET desc = :desc
                     WHERE trans_id = :trans_id""",
                     {'table': table, 'desc': desc, 'trans_id': trans.trans_id})
 
 def remove_trans(trans, table):
     with conn:
-        c.connect(""" DELETE from :table WHERE trans_id = :trans_id""",
+        c.execute(""" DELETE from :table WHERE trans_id = :trans_id""",
                     {'table': table, 'trans_id': trans.trans_id})
 
 def get_trans_by_date(date, table):
-    c.connect("SELECT * FROM :table WHERE date = :date", {'table': table, 'date': date})
+    c.execute("SELECT * FROM :table WHERE date = :date", {'table': table, 'date': date})
     return c.fetchall()
 
 def get_trans_overunder_amount(amount,over_under ,table):
@@ -62,14 +62,14 @@ def get_trans_overunder_amount(amount,over_under ,table):
         over_under = ">"
     else:
         over_under = "<"
-    c.connect("SELECT * FROM :table WHERE amount :over_under :amount",
+    c.execute("SELECT * FROM :table WHERE amount :over_under :amount",
             {'table': table, 'over_under': over_under, 'amount': amount})
     return c.fetchall()
 
 def get_trans_by_id(trans_id, table):
-    c.connect("SELECT * FROM :table WHERE trans_id = :trans_id", {'table': table, 'trans_id': trans_id})
+    c.execute("SELECT * FROM :table WHERE trans_id = :trans_id", {'table': table, 'trans_id': trans_id})
     return c.fetchall()
 
 def get_trans_by_table(table):
-    c.connect("SELECT * FROM :table WHERE table = :table", {'table': table})
+    c.execute("SELECT * FROM :table WHERE table = :table", {'table': table})
     return c.fetchall()

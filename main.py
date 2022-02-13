e708aee804926a4984dd5772f590b6b95d726dba
plaughlin98 <plaughlin98@gmail.com>
2022-02-13 11:05:57 -0600

    Refactored and Tidied Main.py functions

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dateutil import parser
 from matplotlib import style
 style.use('fivethirtyeight')
 
-### TEMP DATA
+### TEMP DUMMY DATA
 exp1 = Transaction("2022-04-12", 530, "Books", "Trans1", "expense")
 exp2 = Transaction("2022-10-12", 621, "Books", "Trans1", "expense")
 
@@ -19,11 +19,15 @@ inc2 = Transaction("2022-02-12", 896, "Books", "Trans1", "income")
 
 # INSERT'finances.db' AFTER TESTING
 # INSERT ':memory:' for testing
-
 conn = sqlite3.connect(':memory:')
 c = conn.cursor()
-# CREATE TABLES
 
+
+#**************************************************************************************
+#***************************** SETUP & CONFIG FUNCTIONS *******************************
+#**************************************************************************************
+
+# CREATE TABLES
 #USERS
 c.execute("""CREATE TABLE IF NOT EXISTS users (
         user text,
@@ -40,10 +44,12 @@ def create_user_trans_tbl(user):
                 trans_type text
                 )""".format(user))
 
+# GET ALL CURRENT USERS
 def get_users():
     c.execute("SELECT * FROM users")
     return c.fetchone()
 
+# GENERATE ID FOR TRANSACTION
 def get_next_id(user):
     table = user + "_transactions"
     c.execute("""SELECT * FROM {}
@@ -66,6 +72,25 @@ def get_new_trans_info(user):
 
     return Transaction(trans_date, amount, category, trans_id, trans_type)
 
+
+def display(trans_get, count):
+    if trans_get is None:
+        print("That is not a valid Query")
+    if count > 1:
+        for item in trans_get:
+            print(item)
+    else:
+        print(trans_get)
+
+#**************************************************************************************
+#*************************** SETUP & GENERAL FUNCTIONS END ****************************
+#**************************************************************************************
+
+
+#**************************************************************************************
+#***************************** UPDATE /REMOVE QUERIES *********************************
+#**************************************************************************************
+
 def insert_trans(trans, table):
     """trans -> Transaction Class Instance
     """
@@ -75,59 +100,99 @@ def insert_trans(trans, table):
         
         conn.commit()
 
-## TO-DO: REFACTOR CODE INTO LESS FUNCTIONS
-
 def update_trans(table, trans_id, item, value):
     with conn:
-        c.execute(""" UPDATE {} SET :item = :value
-                    WHERE trans_id = :trans_id""".format(table),
-                    {'trans_id': trans_id, 'item': item, 'value': value})
+        c.execute(""" UPDATE {} SET {} = :value
+                    WHERE trans_id = :trans_id""".format(table, item),
+                    {'trans_id': trans_id, 'value': value})
 
-# def update_amount(trans, amount, table):
-#     with conn:
-#         c.execute("""UPDATE :table SET amount = :amount
-#                     WHERE trans_id = :trans_id""",
-#                     {'table': table, 'trans_id': trans.trans_id, 'amount': amount})
-
-# def update_date(trans, trans_date, table):
-#     with conn:
-#         c.execute("""UPDATE :table SET trans_date = :trans_date
-#                     WHERE trans_id = :trans_id""",
-#                     {'table': table, 'trans_date': trans_date, 'trans_id': trans.trans_id})
-
-# def update_category(trans, category, table):
-#     with conn:
-#         c.execute(""" UPDATE :table SET category = :category
-#                     WHERE trans_id = :trans_id""",
-#                     {'table': table, 'category': category, 'trans_id': trans.trans_id})
+        conn.commit()
 
 def remove_trans(trans_id, table):
     with conn:
         c.execute(""" DELETE from {} WHERE trans_id = :trans_id""".format(table),
                     {'trans_id': trans_id})
+        conn.commit()
 
-def get_trans_by_date(trans_date, table):
-    c.execute("SELECT * FROM {} WHERE trans_date = :trans_date".format(table), {'trans_date': trans_date})
-    return c.fetchall()
+#**************************************************************************************
+#***************************** UPDATE /REMOVE QUERIES END *****************************
+#**************************************************************************************
 
-def get_trans_overunder_value(value, over_under ,table):
+#**************************************************************************************
+#***************************** SELECT QUERIES *****************************************
+#**************************************************************************************
+
+def get_account_balance(table):
+    c.execute("SELECT amount FROM {} WHERE trans_type = expense".format(table))
+    expenses = c.fetchall()
+    total_expenses = 0
+
+    for expense in expenses:
+        total_expenses += expense
+
+    c.execute("SELECT amount FROM {} WHERE trans_type = income".format(table))
+    incomes = c.fetchall()
+
+    total_incomes = 0
+    for income in incomes:
+        total_incomes += income
+
+    print(total_incomes - total_expenses)
+
+def get_spec_trans(item, value, table):
+    c.execute("SELECT COUNT(*) FROM {}".format(table))
+    for i in c.fetchone():
+        count = i
+
+    c.execute("SELECT * FROM {} WHERE {} = :value".format(table, item), { 'value': value})
+    
+    if count == 1:
+        display_get = c.fetchone()
+    else:
+        display_get = c.fetchall()
+
+    display(display_get, count)
+
+def get_over_under_value(value, item, over_under,table):
+    c.execute("SELECT COUNT(*) FROM {}".format(table))
+    for i in c.fetchone():
+        count = i
+
     over_under = over_under.lower()
     if over_under == "over":
         over_under = ">"
     else:
         over_under = "<"
-    c.execute("SELECT * FROM {} WHERE amount :over_under :amount".format(table),
-            {'over_under': over_under, 'amount': value})
-    return c.fetchall()
+    c.execute("SELECT * FROM {} WHERE :item :over_under :value".format(table),
+            {'item': item, 'over_under': over_under, 'value': value})
 
-def get_trans_by_id(trans_id, table):
-    c.execute("SELECT * FROM {} WHERE trans_id = :trans_id".format(table), {'trans_id': trans_id})
-    return c.fetchall()
+    if count == 1:
+        display_get = c.fetchone()
+    else:
+        display_get = c.fetchall()
 
+    display(display_get, count)
 
-def get_trans_by_table(table):
+def get_all_trans(table):
+    c.execute("SELECT COUNT(*) FROM {}".format(table))
+    for i in c.fetchone():
+        count = i
+        
     c.execute("SELECT * FROM {}".format(table))
-    return c.fetchall()
+    if count == 1:
+        display_get = c.fetchone()
+    else:
+        display_get = c.fetchall()
+
+    display(display_get, count)
+
+#**************************************************************************************
+#***************************** SELECT QUERIES END *************************************
+#**************************************************************************************
+
+#**************************************************************************************
+#********************************** GRAPHING ******************************************
+#**************************************************************************************
 
 # TO-DO: FIX GRAPH FORMATTING AND ALLOW USER TO DYNAMICALLY CHOOSE WHAT TO GRAPH
 def graph_data(table):
@@ -149,34 +214,28 @@ def graph_data(table):
     plt.ylabel("Transaction Amount")
     plt.show()
 
+#**************************************************************************************
+#******************************* GRAPHING END *****************************************
+#**************************************************************************************
+
+
+# TO-DO: CREATE INPUT LOOP TO DYNAMICALLY GO THROUGH ALL FUNCTIONS BASED ON USER INPUT
 
 def main_loop():
-    print("Hello, Welcome")
+    #print("Hello, Welcome")
     user = input("Please Enter your username: ")
+    user_table = user + "_transactions"
     user_list = get_users()
-    print(user_list)
 
     if not user_list or user not in user_list:
         #Make Input Prompt later  
         print("Looks like you don't have an account, would you like to create one?")
         create_user_trans_tbl(user)
-    user_table = user + "_transactions"
     print("Hello {}".format(user))
 
-    # DUMMY DATA // DELETE
-    insert_trans(get_new_trans_info(user), user_table)
-    # insert_trans(exp1, user_table)
-    # insert_trans(exp2, user_table)
-    # insert_trans(inc1, user_table)
-    # insert_trans(inc2, user_table)
-    print(get_trans_by_table(user_table))
-    graph_data(user_table)
-
-
-# insert_trans(exp1, "expenses")
-# insert_trans(exp1, "expenses")
-# insert_trans(inc1, "income")
-# insert_trans(inc2, "income")
+    insert_trans(exp1, user_table)
+    get_all_trans(user_table)
+    get_spec_trans('category', 'Books', user_table)
 
 
 main_loop()
